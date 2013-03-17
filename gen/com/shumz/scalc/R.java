9c7583333f180df362ab30019b642fb494865ca4
Izek Schum <izek.schum@gmail.com>
2013-03-17 14:14:54 +0200

    0006. Project General Fixes was made.

--- gen/com/shumz/scalc/R.java
+++ gen/com/shumz/scalc/R.java
@@ -68,8 +68,9 @@ public final class R {
         public static final int tvResult=0x7f0a0004;
     }
     public static final class layout {
-        public static final int activity_scalc_activity=0x7f030000;
-        public static final int activity_splash=0x7f030001;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_scalc_activity=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f090000;
